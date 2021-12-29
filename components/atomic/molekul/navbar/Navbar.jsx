08c301d401b4ba6609ace6f31365bb71cdee4c41
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-12-29 12:13:26 +0700

    button login

--- components/atomic/molekul/navbar/Navbar.jsx
+++ components/atomic/molekul/navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             <div className="w-4/12 ">
               <div className="flex gap-2 justify-end">
                 <button className="lg:block text-white px-7 hidden capitalize">sign in</button>
-                <button className="lg:block bg-[#c45719] hover:bg-red-500 px-7 py-2 capitalize text-white hidden rounded-sm">sign up</button>
+                <button className="lg:block bg-[#c45719] hover:bg-red-500 px-10 py-3 capitalize text-white hidden rounded-sm font-bold">sign up</button>
                 <button className='lg:hidden border-gray-50 cursor-pointer  lg:static top-9 bg-gradient p-1 z-40' onClick={() => setShowMenu(!showMenu)}>{showMenu ? (<RiMenu3Line color='#fff' size={35} />) : (<RiCloseLine color='#fff' size={35}/>)}</button>
               </div>
               
